53fc5ed72095597adc7e440909dfa62889555fb6
Robert Walker <robert4723@me.com>
2011-01-11 18:03:25 -0500

    Initial import.

---
+++ CoreDataVersioning_AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  CoreDataVersioning_AppDelegate.h
+//  CoreDataVersioning
+//
+//  Created by Robert Walker on 1/11/11.
+//  Copyright Robert Walker 2011 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CoreDataVersioning_AppDelegate : NSObject 
+{
+    NSWindow *window;
+    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;
+}
+
+@property (nonatomic, retain) IBOutlet NSWindow *window;
+
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:sender;
+
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CoreDataVersioning
+//
+//  Created by Robert Walker on 1/11/11.
+//  Copyright Robert Walker 2011. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
